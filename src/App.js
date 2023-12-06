47d92e589506e8e8ac903cebe21d40bf597c84c5
Vitória Solange <you@example.com>
2023-12-06 13:11:52 -0300

    Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import { FiSearch } from "react-icons/fi";
-import './styles.css'
-
+import './styles.css';
 
 function App() {
+  const [input, setInput] = useState('')
+  
+  function handleSearch() {
+    alert("CLICOU" + input)
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador Cep</h1>
@@ -11,20 +17,23 @@ function App() {
         <input 
         type="text"
         placeholder="Digite seu cep..."
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
         />
 
-        <button className="ButtonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="white"/>
         </button>
       </div>
 
       <main className="main">
-        <h2>CEP:79003222</h2>
+        <h2>CEP:01001-000</h2>
 
-        <span>Rua Teste</span>
-        <span>Complemento: Algum</span>
-        <span>Vila SSJ</span>
-        <span>Fortaleza</span>
+        <span>Logradouro: "Praça da Sé"</span>
+        <span>Complemento: "lado ímpar"</span>
+        <span>Bairro: "Sé"</span>
+        <span>Localidade: "São Paulo"</span>
+        <span>Uf: "SP"</span>
 
       </main>
 
